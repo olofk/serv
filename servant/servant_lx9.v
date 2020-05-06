794748dac4d318643e6b360b08d83a332cc6b7fd
Olof Kindgren <olof.kindgren@gmail.com>
2020-05-06 20:51:55 +0200

    Add support for LX9 Microboard

---
+++ servant/servant_lx9.v
@@ -0,0 +1,32 @@
+`default_nettype none
+module servant_lx9
+(
+ input wire  i_clk,
+ input wire  i_rst,
+ output wire o_uart_tx,
+ output wire q);
+
+   parameter memfile = "zephyr_hello.hex";
+   parameter memsize = 8192;
+
+   wire      wb_clk;
+   wire      wb_rst;
+
+   assign o_uart_tx = q;
+
+   servant_lx9_clock_gen
+   clock_gen
+     (.i_clk (i_clk),
+      .i_rst (i_rst),
+      .o_clk (wb_clk),
+      .o_rst (wb_rst));
+
+   servant
+     #(.memfile (memfile),
+       .memsize (memsize))
+   servant
+     (.wb_clk (wb_clk),
+      .wb_rst (wb_rst),
+      .q      (q));
+
+endmodule
