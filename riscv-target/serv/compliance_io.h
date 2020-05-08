731ea3cc40646b24591605f1ecc86db59d4c529f
Olof Kindgren <olof.kindgren@gmail.com>
2020-05-08 22:56:31 +0200

    Align RISC-V compliance test target code with upstream changes

--- riscv-target/serv/compliance_io.h
+++ riscv-target/serv/compliance_io.h
@@ -27,9 +27,9 @@
 //-----------------------------------------------------------------------
 
 #define RVTEST_IO_INIT
-#define RVTEST_IO_WRITE_STR(_STR)
+#define RVTEST_IO_WRITE_STR(_SP, _STR)
 #define RVTEST_IO_CHECK()
-#define RVTEST_IO_ASSERT_GPR_EQ(_R, _I)
+#define RVTEST_IO_ASSERT_GPR_EQ(_SP, _R, _I)
 #define RVTEST_IO_ASSERT_SFPR_EQ(_F, _R, _I)
 #define RVTEST_IO_ASSERT_DFPR_EQ(_D, _R, _I)
 
