52d0bf09380d5673308fdc2e423e448b6e2c9abb
Klas Nordmark <klas.nordmark.se@ieee.org>
2021-03-30 09:42:00 +0200

    Added openlane target and params.tcl with suitable openlane parameters for SERV

---
+++ data/params.tcl
@@ -0,0 +1,6 @@
+set ::env(CLOCK_PERIOD) "10"
+set ::env(CLOCK_PORT) "clk"
+set ::env(DIE_AREA) "0 0 200 200"
+set ::env(FP_SIZING) absolute
+set ::env(DESIGN_IS_CORE) 0
+set ::env(GLB_RT_MAXLAYER) 5
