5c303f46b44863367d3cbcff6f95ebbfcf890532
Stefan Wallentowitz <stefan.wallentowitz@hm.edu>
2021-04-18 13:58:43 +0200

    Build website automatically and publish to gh-pages

--- doc/conf.py
+++ doc/conf.py
@@ -52,4 +52,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_extra_path = ['.nojekyll']
+
 master_doc = 'index'
+
+online_wavedrom_js_url = "https://wavedrom.com"
