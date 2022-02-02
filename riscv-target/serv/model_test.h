d4f28dae526f18464928f207967dba60be4a5a2a
Usman <usmanzain95@gmail.com>
2022-02-02 19:17:02 +0500

    removed empty spaces

--- riscv-target/serv/model_test.h
+++ riscv-target/serv/model_test.h
@@ -56,26 +56,16 @@ _start:
 
 
 #define LOCAL_IO_WRITE_STR(_STR) RVMODEL_IO_WRITE_STR(x31, _STR)
-#define RVMODEL_IO_WRITE_STR(_SP, _STR)                                 \
-
-
-#define RSIZE 4
+#define RVMODEL_IO_WRITE_STR(_SP, _STR)
 #define LOCAL_IO_PUSH(_SP)
-
 #define LOCAL_IO_POP(_SP)
-
 #define RVMODEL_IO_ASSERT_GPR_EQ(_SP, _R, _I)
-
 #define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
-
 #define RVMODEL_IO_ASSERT_DFPR_EQ(_D, _R, _I)
 
 #define RVMODEL_SET_MSW_INT
-
 #define RVMODEL_CLEAR_MSW_INT
-
 #define RVMODEL_CLEAR_MTIMER_INT
-
 #define RVMODEL_CLEAR_MEXT_INT
 
 #endif // _COMPLIANCE_MODEL_H
