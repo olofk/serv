5a45f87dfaed4c18213cff6d86dbf5062585b744
Humail Nawaz <179082248+Humail-Nawaz@users.noreply.github.com>
2025-07-08 19:29:22 +0500

    Update ServCore.v

--- rtl/ServCore.v
+++ rtl/ServCore.v
@@ -14,7 +14,7 @@ module ServCore #(
     input  wire rst,
     input  wire i_timer_irq,
 
-    // AXI2WB AXI SIGNALS FROM BRIDGE TO EXTERNAL
+    // WB2AXI AXI SIGNALS FROM BRIDGE TO EXTERNAL
     output wire [AW-1:0]         o_awmaddr,
     output wire                  o_awmvalid,
     input  wire                  i_awmready,
@@ -63,10 +63,62 @@ module ServCore #(
     input  wire                  i_rmvalid,
     output wire                  o_rmready,
     input  wire [ID_WIDTH:0]   i_rm_id,
-    input  wire [USER_WIDTH:0] i_rm_user
+    input  wire [USER_WIDTH:0] i_rm_user,
+
+    // AXI2WB SIGNALS FROM AXI TO SERVING
+    input  wire [AW-1:0] i_awaddr,
+    input  wire          i_awvalid,
+    output wire          o_awready,
+    input  wire [ID_WIDTH:0]    i_awid,
+    input  wire [7:0]           i_awlen,
+    input  wire [2:0]           i_awsize,
+    input  wire [1:0]           i_awburst,
+    input  wire                 i_awlock,
+    input  wire [3:0]           i_awcache,
+    input  wire [2:0]           i_awprot,
+    input  wire [3:0]           i_awqos,
+    input  wire [3:0]            i_awregion,
+    input  wire [USER_WIDTH:0]  i_awuser,
+    input wire [5:0]            i_awtop, 
+    
+    input  wire [AW-1:0] i_araddr,
+    input  wire          i_arvalid,
+    output wire          o_arready,
+    input  wire [ID_WIDTH:0]    i_arid,
+    input  wire [7:0]           i_arlen,
+    input  wire [2:0]           i_arsize,
+    input  wire [1:0]           i_arburst,
+    input  wire                 i_arlock,
+    input  wire [3:0]           i_arcache,
+    input  wire [2:0]           i_arprot,
+    input  wire [3:0]           i_arqos,
+    input  wire [3:0]           i_arregion,
+    input  wire [USER_WIDTH:0]  i_aruser
+
+    
+    input  wire [31:0]   i_wdata,
+    input  wire [3:0]    i_wstrb,
+    input  wire          i_wvalid,
+    output wire          o_wready,
+    input  wire                 i_wlast,
+    input  wire [USER_WIDTH:0]  i_wuser,
+    
+    output wire [1:0]    o_bresp,
+    output wire          o_bvalid,
+    input  wire          i_bready,
+    output wire [ID_WIDTH:0]     o_bid,
+    output wire [USER_WIDTH:0]   o_buser,
+    
+    output wire [31:0]   o_rdata,
+    output wire [1:0]    o_rresp,
+    output wire          o_rlast,
+    output wire          o_rvalid,
+    input  wire          i_rready,
+    output wire [ID_WIDTH:0]     o_rid,
+    output wire [USER_WIDTH:0]   o_ruser    
 );
 
-    // Internal Wishbone signals (SERV <-> BRIDGE)
+    // Internal Wishbone interface (SERV <-> Bridge)
     wire [AW-1:2] i_swb_adr;
     wire [31:0]   i_swb_dat;
     wire [3:0]    i_swb_sel;
@@ -75,7 +127,25 @@ module ServCore #(
     wire [31:0]  o_swb_rdt;
     wire         o_swb_ack;
 
-    // Tie off unused AXI signals
+    // External Wishbone interface (Bridge <-> SERV)
+    wire [AW-1:2] o_mwb_adr;
+    wire [31:0]   o_mwb_dat;
+    wire [3:0]    o_mwb_sel;
+    wire         o_mwb_we;
+    wire         o_mwb_stb;
+    wire [31:0]   i_mwb_rdt;
+    wire          i_mwb_ack;
+
+    // Bridge <-> SERV mux control
+    wire sel_wadr, sel_wdata, sel_radr, sel_rdata, sel_wen;
+
+       // Tie off unused AXI signals
+    assign o_bid=1'b0;
+    assign o_buser=1'b0;
+
+    assign o_rid=1'b0;
+    assign o_ruser=1'b0;
+    
     assign o_awm_id     = 1'b0;
     assign o_awm_len    = 8'b0;
     assign o_awm_size   = 3'b0;
@@ -90,6 +160,7 @@ module ServCore #(
 
     assign o_wm_last    = 1'b0;
     assign o_wm_user    = 1'b0;
+    
     assign o_arm_id     = 1'b0;
     assign o_arm_len    = 8'b0;
     assign o_arm_size   = 3'b0;
@@ -112,19 +183,40 @@ module ServCore #(
         .i_clk(clk),
         .i_rst(rst),
         .i_timer_irq(i_timer_irq),
+
+        // Master WB (SERV → Bridge)
+        .o_wb_addr(), // optional full address
         .o_wb_adr(i_swb_adr),
         .o_wb_dat(i_swb_dat),
         .o_wb_sel(i_swb_sel),
         .o_wb_we(i_swb_we),
         .o_wb_stb(i_swb_stb),
         .i_wb_rdt(o_swb_rdt),
-        .i_wb_ack(o_swb_ack)
+        .i_wb_ack(o_swb_ack),
+
+        // Slave WB (Bridge → SERV)
+        .adr_brg(o_mwb_adr),
+        .data_brg(o_mwb_dat),
+        .stb_brg(o_mwb_stb),
+        .wen_brg(o_mwb_we),
+        .sel_brg(o_mwb_sel),
+        .rdt_brg(i_mwb_rdt),
+        .ack_brg(i_mwb_ack),
+
+        // mux selection signals from bridge
+        .sel_wadr(sel_wadr),
+        .sel_wdata(sel_wdata),
+        .sel_radr(sel_radr),
+        .sel_rdata(sel_rdata),
+        .sel_wen(sel_wen)
     );
 
     // Instantiate AXI-Wishbone bridge
-    complete_bridge #(.AW(AW)) uut (
+    complete_bridge #(.AW(AW)) bridge (
         .i_clk(clk),
         .i_rst(rst),
+
+        // Wishbone slave (SERV master → Bridge)
         .i_swb_adr(i_swb_adr),
         .i_swb_dat(i_swb_dat),
         .i_swb_sel(i_swb_sel),
@@ -132,6 +224,37 @@ module ServCore #(
         .i_swb_stb(i_swb_stb),
         .o_swb_rdt(o_swb_rdt),
         .o_swb_ack(o_swb_ack),
+
+        // Wishbone master (Bridge → SERV slave)
+        .o_mwb_adr(o_mwb_adr),
+        .o_mwb_dat(o_mwb_dat),
+        .o_mwb_sel(o_mwb_sel),
+        .o_mwb_we(o_mwb_we),
+        .o_mwb_stb(o_mwb_stb),
+        .i_mwb_rdt(i_mwb_rdt),
+        .i_mwb_ack(i_mwb_ack),
+
+        // AXI slave (external → bridge)
+        .i_awaddr(i_awaddr),
+        .i_awvalid(i_awvalid),
+        .o_awready(o_awready),
+        .i_araddr(i_araddr),
+        .i_arvalid(i_arvalid),
+        .o_arready(o_arready),
+        .i_wdata(i_wdata),
+        .i_wstrb(i_wstrb),
+        .i_wvalid(i_wvalid),
+        .o_wready(o_wready),
+        .o_bresp(o_bresp),
+        .o_bvalid(o_bvalid),
+        .i_bready(i_bready),
+        .o_rdata(o_rdata),
+        .o_rresp(o_rresp),
+        .o_rlast(o_rlast),
+        .o_rvalid(o_rvalid),
+        .i_rready(i_rready),
+
+        // AXI master (bridge → external)
         .o_awmaddr(o_awmaddr),
         .o_awmvalid(o_awmvalid),
         .i_awmready(i_awmready),
@@ -149,8 +272,18 @@ module ServCore #(
         .i_rmresp(i_rmresp),
         .i_rmlast(i_rmlast),
         .i_rmvalid(i_rmvalid),
-        .o_rmready(o_rmready)
+        .o_rmready(o_rmready),
+
+        // mux selection outputs
+        .sel_wadr(sel_wadr),
+        .sel_wdata(sel_wdata),
+        .sel_radr(sel_radr),
+        .sel_rdata(sel_rdata),
+        .sel_wen(sel_wen)
     );
 
 endmodule
 
+ 
+
+   
