8848e7bae86f0bcb22c24b8e5f7431ab38432167
Leonardo Capossio <capossio.leonardo@gmail.com>
2026-03-03 17:50:14 +0800

    verif: add RISCOF plugin for RV32E compliance testing
    Adds a self-contained RISCOF configuration that runs the riscv-arch-test
    RV32E suite against SERV with WITH_RV32E=1 and compares signatures with
    the SAIL reference model.
    
    New files:
    - config_rv32e.ini: top-level RISCOF config; DUT=serv_rv32e,
      reference=sail_cSim (same reference model as RV32I run)
    
    - plugin-serv/serv_isa_rv32e.yaml: ISA spec for RV32E_Zicsr;
      misa reset-val=0x40000010 (MXL=01, E-bit=1)
    
    - plugin-serv/riscof_serv_rv32e.py: DUT plugin derived from
      riscof_serv.py with three key changes:
      * Builds servant with --with_rv32e=1 into servant_test_rv32e/
        (separate work root to avoid clobbering the RV32I build)
      * Uses -mabi=ilp32e and constructs isa as rv32e[_zicsr]
      * Includes env_rv32e/ instead of env/
    
    - plugin-serv/env_rv32e/link.ld: identical to env/link.ld (same
      SERV memory layout, no address changes needed for RV32E)
    
    - plugin-serv/env_rv32e/model_test.h: RV32E-safe version of the
      compliance test macros.  The RV32I version uses a6 (x16) as a
      scratch register in RVMODEL_HALT to hold the constant 10 for
      hex-nibble comparison.  In RV32E mode x16 maps silently to x0
      (always 0), so 'blt a5, a6' would never branch and every nibble
      would be mis-encoded.  Fixed by replacing:
        li   a6, 10 / blt  a5, a6, target
      with:
        slti t1, a5, 10 / bnez t1, target
      using t1 (x6), which is within the x0-x15 RV32E register file.
    
    Run with:
      riscof run --config=$SERV/verif/config_rv32e.ini     --suite=riscv-arch-test/riscv-test-suite/rv32e_m/E     --env=riscv-arch-test/riscv-test-suite/env

---
+++ verif/plugin-serv/riscof_serv_rv32e.py
@@ -0,0 +1,100 @@
+import os
+import logging
+
+import riscof.utils as utils
+from riscof.pluginTemplate import pluginTemplate
+
+logger = logging.getLogger()
+
+class serv_rv32e(pluginTemplate):
+    __model__ = "serv_rv32e"
+    __version__ = "1.4.0"
+
+    def __init__(self, *args, **kwargs):
+        sclass = super().__init__(*args, **kwargs)
+        config = kwargs.get('config')
+        config_dir = kwargs.get('config_dir')
+        if config is None:
+            print("Please enter input file paths in configuration.")
+            raise SystemExit(1)
+        self.pluginpath=os.path.join(config_dir, config['pluginpath'])
+        self.isa_spec = os.path.join(config_dir, config['ispec'])
+        self.platform_spec = os.path.join(config_dir, config['pspec'])
+        if 'target_run' in config and config['target_run']=='0':
+            self.target_run = False
+        else:
+            self.target_run = True
+        return sclass
+
+    def initialise(self, suite, work_dir, archtest_env):
+       self.work_dir = work_dir
+       self.suite_dir = suite
+       # Use env_rv32e/ which has a model_test.h restricted to x0-x15
+       self.compile_cmd = 'riscv64-unknown-elf-gcc -march={0} \
+         -static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g\
+         -T '+self.pluginpath+'/env_rv32e/link.ld\
+         -I '+self.pluginpath+'/env_rv32e/\
+         -I ' + archtest_env + ' {1} -o {2} {3}'
+
+       # Build an RV32E-enabled servant simulator into a separate work root
+       # so it does not overwrite the RV32I build.
+       # Paths are relative to CWD; riscof must be run from the repo root.
+       build_serv = 'fusesoc run --target=verilator_tb\
+         --build --work-root=servant_test_rv32e award-winning:serv:servant\
+         --memsize=8388608 --with_rv32e=1'
+       utils.shellCommand(build_serv).run()
+
+    def build(self, isa_yaml, platform_yaml):
+      ispec = utils.load_yaml(isa_yaml)['hart0']
+      self.xlen = '32'
+      self.isa = 'rv32e'
+      if "Zicsr" in ispec["ISA"] or "zicsr" in ispec["ISA"]:
+          self.isa += '_zicsr'
+      # ilp32e ABI: integer registers x0-x15 only
+      self.compile_cmd = self.compile_cmd + ' -mabi=ilp32e '
+
+    def runTests(self, testList):
+      for testentry in testList.values():
+          test = testentry['test_path']
+          test_dir = testentry['work_dir']
+          file_name = os.path.basename(test)[:-2]
+
+          elf = file_name+'.elf'
+          compile_macros= ' -D' + " -D".join(testentry['macros'])
+          marchstr = testentry['isa'].lower()
+
+          compile_run = self.compile_cmd.format(marchstr, test, elf, compile_macros)
+          utils.shellCommand(compile_run).run(cwd=test_dir)
+
+          objcopy_run = f'riscv64-unknown-elf-objcopy -O binary {elf} {file_name}.bin'
+          utils.shellCommand(objcopy_run).run(cwd=test_dir)
+
+          self.makehex(f"{test_dir}/{file_name}.bin", f"{test_dir}/{file_name}.hex")
+
+          # Relative to repo root (where riscof must be invoked from)
+          exe = 'servant_test_rv32e/Vservant_sim'
+
+          sigdump_run = [exe,
+                         "+timeout=1000000000",
+                         f"+signature={test_dir}/DUT-serv.signature",
+                         f"+firmware={test_dir}/{file_name}.hex"]
+
+          utils.shellCommand(' '.join(sigdump_run)).run()
+      if not self.target_run:
+          raise SystemExit
+
+    def makehex(self, binfile, hexfile):
+        with open(binfile, "rb") as f, open(hexfile, "w") as fout:
+            cnt = 3
+            s = ["00"]*4
+            while True:
+                data = f.read(1)
+                if not data:
+                    fout.write(''.join(s)+'\n')
+                    return
+                s[cnt] = "{:02X}".format(data[0])
+                if cnt == 0:
+                    fout.write(''.join(s)+'\n')
+                    s = ["00"]*4
+                    cnt = 4
+                cnt -= 1
